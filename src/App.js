0ef72105afd754ff6bdec156aeeca769c3a28f94
Mario Arro <marioarro186@gmail.com>
2022-08-29 09:25:48 +0300

    starting update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [currentVal, setCurrentVal] = useState('X');
+  const [table, setTable] = useState([
+    {id:1, value:''},{id:2, value:''},{id:3, value:''},{id:4, value:''},{id:5, value:''},{id:6, value:''},{id:7, value:''},{id:8, value:''},{id:9, value:''},
+  ])
+  // const currentVal = 'X';
+  // const table = [
+  //   {id:1, value:''},{id:2, value:''},{id:3, value:''},{id:4, value:''},{id:5, value:''},{id:6, value:''},{id:7, value:''},{id:8, value:''},{id:9, value:''},
+  // ];
+
+
+
+  const changeCell = (id) => {
+    setTable(table.map((cell) => cell.id !== id ? cell : {id: id, value: currentVal}))
+    setCurrentVal(currentVal === 'X' ? 'O' : 'X')
+  }
+  
+
+  // useEffect(() => {
+  //   console.log(table)
+  // }, [table])
+  
+console.log(table)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="game-container">
+        {table.map((cell) => <div onClick={() => changeCell(cell.id)} key={cell.id} className="cell">{cell.value}</div>)}
+      </div>
     </div>
   );
 }
